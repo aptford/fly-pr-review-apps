41a41feaab955ac2c17c898129f4a17abe8b0923
sasha <17913919+aptlin@users.noreply.github.com>
2024-05-06 18:09:14 -0400

    fix: attach postgres once

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,7 +63,7 @@ if [ -n "$INPUT_SECRETS" ]; then
 fi
 
 # Attach postgres cluster to the app if specified.
-if [ -n "$INPUT_POSTGRES" ]; then
+if [ -n "$INPUT_POSTGRES" ] && ! flyctl status --app "$app"; then
   flyctl postgres attach "$INPUT_POSTGRES" --app "$app" || true
 fi
 
@@ -72,10 +72,8 @@ echo "Contents of config $config file: " && cat "$config"
 if [ -n "$INPUT_VM" ]; then
   flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE" --remote-only
 
-  statusmessage="Review app created. It may take a few minutes for the app to deploy."
-elif [ "$EVENT_TYPE" = "synchronize" ]; then
+else
   flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY" --remote-only
-  statusmessage="Review app updated. It may take a few minutes for your changes to be deployed."
 fi
 
 flyctl status --app "$app" --json >status.json
